db0812c27be7c71a77af575f2333bbef0318030e
Mahamud Hasan Emon <imonmahamud495@gmail.com>
2025-12-31 13:32:12 +0600

    conditional statement

---
+++ condition.py
@@ -0,0 +1,40 @@
+# age=40
+# if age>50:
+#     print("You are old guy dadu!")
+# else:
+#     print("You are young enough!")
+#check a number take input and make decision is it even or add;
+
+# num=int(input('Give a number as a input : '))
+# if num%2==0:
+#     print('This is a even number')
+# else:
+#     print('This is a odd numebr')
+
+# num=int(input('Enter a Number : '))
+
+# if num>0:
+#     print('This is a positive Number')
+# elif num<0:
+#     print('This is a Negative number')
+# else:
+#     print('This is a zero')
+
+# age=int(input('Enter your age : '))
+# if age>20 and age<30:
+#     print('You are capable for this jobs!')
+# else:
+#     print('Sorry! you are not capable for this jobs!')
+
+
+
+num=int(input('Enter a number: '))
+if num>0:
+    if num%2==0:
+        print('This is a positive and even number')
+    else:
+        print('positive odd number')
+elif num<0:
+    print('Negative Number')
+else:
+    print('This is zero.')
