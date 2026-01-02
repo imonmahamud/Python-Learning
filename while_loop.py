ceb95c1d1fc4eb01cc8263fd460b62b8c5912671
Mahamud Hasan Emon <imonmahamud495@gmail.com>
2026-01-02 16:21:05 +0600

    count sum

--- while_loop.py
+++ while_loop.py
@@ -9,12 +9,26 @@
 
 # print(result)
 
-a=[-10,2,19,-3,-5]
+# a=[-10,2,19,-3,-5]
 
-i=0
-while i<len(a):
-    if a[i]<0:
-        a[i]=0
-    i=i+1
+# i=0
+# while i<len(a):
+#     if a[i]<0:
+#         a[i]=0
+#     i=i+1
 
-print(a)
+# print(a)
+
+# count=list(range(1,11,1))
+# for i in count:
+#     print(i)
+
+# print('the end')
+
+count=1
+sum=0
+while count<=10:
+    sum=sum+count
+    count+=1
+
+print(sum)
